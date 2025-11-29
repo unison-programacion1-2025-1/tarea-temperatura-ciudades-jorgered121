f17213042a54a9716d64fe7cf1483097e6a34c3b
jorgered121 <jorge.luis.sanchez.rojo@gmail.com>
2025-11-28 21:22:29 -0700

    Update main.py

--- main.py
+++ main.py
@@ -32,8 +32,11 @@ print(f"El día con la temperatura mínima en Phoenix fue: {fecha_min_phoenix}")
 # TODO: Imprime la temperatura mínima en Phoenix con el siguiente mensaje: "La temperatura mínima registrada en Phoenix fue de: ", temperatura, " °C""
 print(f"La temperatura mínima registrada en Phoenix fue de: {min_temp_phoenix} °C")
 # TODO: Imprime que día y hora se registró la temperatura máxima en Phoenix con el siguiente mensaje: "El día con la temperatura máxima en Phoenix fue: {fecha}"
+max_temp_phoenix = df_celsius['Phoenix'].max().round(2)
+fecha_max_phoenix = df_celsius['Phoenix'].idxmax()
+print(f"El día con la temperatura mínima en Phoenix fue: {fecha_max_phoenix}")
 # TODO: Imprime la temperatura máxima en Phoenix con el siguiente mensaje: "La temperatura máxima registrada en Phoenix fue de: ", temperatura, " °C""
-
+print(f"La temperatura máxima registrada en Phoenix fue de: {max_temp_phoenix} °C")
 # TODO: Imprime la temperatura promedio en Phoenix durante el año 2016 con el siguiente mensaje: "La temperatura promedio durante 2016 en Phoenix fue de: ", temperatura, " °C""
 
 # Graficar la temperatura de Phoenix durante el año 2016
